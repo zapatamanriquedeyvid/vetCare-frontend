1fcc6f20c436cf519824e5e664dbaf120367d161
zapatamanriquedeyvid <zapatamanriquedeyvid@gmail.com>
2026-04-29 18:02:23 -0500

    Update API base URL to production environment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: "https://adventurous-purpose-production.up.railway.app/api/",
-  baseURL: "http://localhost:8080/api/", 
+  baseURL: "https://adventurous-purpose-production.up.railway.app/api/",
+  //baseURL: "http://localhost:8080/api/", 
 });
 
 export default api;
